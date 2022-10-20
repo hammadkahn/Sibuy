2281289116abe7da0a083527a49c415faac4bac1
Hammad khan <hammad.khan0312@gmail.com>
2022-10-20 11:46:53 +0500

    theme and logo chaanged

--- lib/user_app/splash_screen/splashh.dart
+++ lib/user_app/splash_screen/splashh.dart
@@ -12,26 +12,73 @@ class user_splash extends StatefulWidget {
   State<user_splash> createState() => _user_splashState();
 }
 
-class _user_splashState extends State<user_splash> {
+class _user_splashState extends State<user_splash>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
   @override
   void initState() {
     super.initState();
-    Timer(
-        const Duration(seconds: 3),
-        () => Navigator.of(context).pushReplacement(MaterialPageRoute(
-            builder: (BuildContext context) => User_onboard())));
+    //navigate to next page after 5 seconds
+    // Timer(const Duration(seconds: 5), () {
+    //   Navigator.pushReplacement(context,
+    //       MaterialPageRoute(builder: (context) => const User_onboard()));
+    // });
+    animationController = new AnimationController(
+      vsync: this,
+      duration: new Duration(seconds: 5),
+    );
+
+    animationController.repeat();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Image.asset(
-          'assets/images/gigi-logo.png',
-          width: 100,
-          height: 100,
+    return new Container(
+      alignment: Alignment.center,
+      color: Colors.white,
+      child: new AnimatedBuilder(
+        animation: animationController,
+        child: new Container(
+          height: 150.0,
+          width: 150.0,
+          child: new Image.asset('assets/images/gigi-logo.png'),
         ),
+        builder: (BuildContext context, Widget? _widget) {
+          return new Transform.rotate(
+            angle: animationController.value * 10,
+            child: _widget,
+          );
+        },
       ),
     );
   }
+  // void initState() {
+  //   super.initState();
+  //   Timer(
+  //       const Duration(seconds: 3),
+  //       () => Navigator.of(context).pushReplacement(MaterialPageRoute(
+  //           builder: (BuildContext context) => User_onboard())));
+  // }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     body: Center(
+  //       //animation of logo turn around
+  //       child: const RotationTransition(
+  //         turns: AlwaysStoppedAnimation(3 / 12),
+  //         child: Image(
+  //           image: AssetImage('assets/images/gigi-logo.png'),
+  //           height: 200,
+  //           width: 200,
+  //         ),
+  //       ),
+  //       // child: Image.asset(
+  //       //   'assets/images/gigi-logo.png',
+  //       //   width: 100,
+  //       //   height: 100,
+  //       // ),
+  //     ),
+  //   );
+  // }
 }
